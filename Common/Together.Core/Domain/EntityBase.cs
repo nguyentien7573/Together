17e4e16b961544bc638aaf21f33ac8987d1a0275
Nguyễn Tiến <61858121+nguyentien7573@users.noreply.github.com>
2022-12-02 19:45:33 +0700

    Update entity Base

--- Common/Together.Core/Domain/EntityBase.cs
+++ Common/Together.Core/Domain/EntityBase.cs
@@ -1,12 +1,14 @@
-﻿namespace Together.Core.Domain
+﻿using Together.Core.Helpers;
+
+namespace Together.Core.Domain
 {
     public class EntityBase
     {
         public Guid Id { get; protected init; } = Guid.NewGuid();
-        public DateTime CreatedOn { get; protected init; } = DateTime.UtcNow;
+        public DateTime CreatedOn { get; protected init; } = DateTimeHelper.NewDateTime();
         public Guid CreatedBy { get; set; }
         public DateTime? UpdatedOn { get; set; }
         public Guid? UpdatedBy { get; set; }
-        public bool isActive { get; set; }
+        public bool Active { get; set; }
     }
 }
