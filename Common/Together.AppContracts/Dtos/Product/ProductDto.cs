f457c941469993efd46c712553cc915e14cecd95
Nguyễn Tiến <61858121+nguyentien7573@users.noreply.github.com>
2022-12-02 19:45:07 +0700

    add App Contracts

---
+++ Common/Together.AppContracts/Dtos/Product/ProductDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Together.AppContracts.Dtos.Product
+{
+    public class ProductDto
+    {
+        public string Name { get; set; } = default!;
+        public bool Active { get; set; }
+        public int Quantity { get; set; }
+        public decimal Cost { get; set; }
+        public Guid ProductCodeId { get; set; }
+      
+        public Guid Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime? UpdatedOn { get; set; }
+    }
+}
