dee1400048d71c75a50cf28808ddcab875686bbe
Nguyễn Tiến <61858121+nguyentien7573@users.noreply.github.com>
2022-12-03 18:23:12 +0700

    Customer Service

---
+++ MicroServices/Customer/Together.CustomerService.Core/Entities/Customer.cs
@@ -0,0 +1,38 @@
+﻿using Together.Core.Domain;
+
+namespace Together.CustomerService.Core.Entities
+{
+    public class Customer : EntityBase
+    {
+        public string FirstName { get; protected set; }
+        public string LastName { get; protected set; }
+        public string Email { get; protected set; }
+
+        public static Customer Create(string firstname, string lastname, string email)
+        {
+            return Create(Guid.NewGuid(), firstname, lastname, email);
+        }
+
+        public static Customer Create(Guid id, string firstname, string lastname, string email)
+        {
+            if (string.IsNullOrEmpty(firstname))
+                throw new ArgumentNullException("firstname");
+
+            if (string.IsNullOrEmpty(lastname))
+                throw new ArgumentNullException("lastname");
+
+            if (string.IsNullOrEmpty(email))
+                throw new ArgumentNullException("email");
+          
+            Customer customer = new()
+            {
+                Id = id,
+                FirstName = firstname,
+                LastName = lastname,
+                Email = email,
+            };
+
+            return customer;
+        }
+    }
+}
---
+++ MicroServices/Customer/Together.CustomerService.Core/UseCases/Commands/CreateCustomer.cs
@@ -0,0 +1,67 @@
+﻿using FluentValidation;
+using MediatR;
+using Together.AppContracts.Dtos.Customer;
+using Together.Core.Domain;
+using Together.Core.Repository;
+using Together.CustomerService.Core.Entities;
+
+namespace Together.CustomerService.Core.UseCases.Commands
+{
+    public class CreateCustomer
+    {
+        public record Command : ICreateCommand<Command.CreateCustomerModel, CustomerDto>
+        {
+            public CreateCustomerModel Model { get; init; } = default!;
+
+            public record CreateCustomerModel(string FirstName, string LastName, string Email, Guid CountryId);
+
+            internal class Validator : AbstractValidator<Command>
+            {
+                public Validator()
+                {
+                    RuleFor(v => v.Model.FirstName)
+                        .NotEmpty().WithMessage("FirstName is required.")
+                        .MaximumLength(50).WithMessage("FirstName must not exceed 50 characters.");
+
+                    RuleFor(v => v.Model.LastName)
+                        .NotEmpty().WithMessage("LastName is required.")
+                        .MaximumLength(50).WithMessage("LastName must not exceed 50 characters.");
+
+                    RuleFor(v => v.Model.Email)
+                        .NotEmpty().WithMessage("Email is required.")
+                        .EmailAddress().WithMessage("Email should in email format.")
+                        .MaximumLength(100).WithMessage("Email must not exceed 100 characters.");
+                }
+            }
+
+            internal class Handler : IRequestHandler<Command, ResultModel<CustomerDto>>
+            {
+                private readonly IRepository<Customer> _customerRepository;
+
+                public Handler(IRepository<Customer> customerRepository)
+                {
+                    _customerRepository = customerRepository ?? throw new ArgumentNullException(nameof(customerRepository));
+                }
+
+                public async Task<ResultModel<CustomerDto>> Handle(Command request,
+                    CancellationToken cancellationToken)
+                {
+                    
+                    var customer = Customer.Create(request.Model.FirstName, request.Model.LastName, request.Model.Email);
+
+                    //customer.AddDomainEvent(new CustomerCreatedIntegrationEvent());
+
+                    var created = await _customerRepository.AddAsync(customer);
+
+                    return ResultModel<CustomerDto>.Create(new CustomerDto
+                    {
+                        Id = created.Id,
+                        FirstName = created.FirstName,
+                        LastName = created.LastName,
+                        Email = created.Email
+                    });
+                }
+            }
+        }
+    }
+}
