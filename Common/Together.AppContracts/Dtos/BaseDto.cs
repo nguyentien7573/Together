69991171bd218f1b2f9eccf7b0211cfded0f6019
Nguyễn Tiến <61858121+nguyentien7573@users.noreply.github.com>
2022-12-27 15:06:55 +0700

    Create BaseDto.cs

---
+++ Common/Together.AppContracts/Dtos/BaseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Together.AppContracts.Dtos
+{
+    public class BaseDto
+    {
+        public DateTime CreatedOn { get; set; }
+        public Guid CreatedBy { get; set; }
+        public DateTime? UpdatedOn { get; set; }
+        public Guid UpdatedBy { get; set; }
+    }
+}
