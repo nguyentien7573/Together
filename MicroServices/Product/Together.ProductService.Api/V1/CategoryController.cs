2792d32574e9bea65a6c8545b7f1480bda12b140
Nguyễn Tiến <61858121+nguyentien7573@users.noreply.github.com>
2022-12-29 17:31:17 +0700

    Create CategoryController.cs

---
+++ MicroServices/Product/Together.ProductService.Api/V1/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Together.Infrastructure.Controller;
+using Together.ProductService.Core.UseCases.Commands;
+using Together.ProductService.Core.UseCases.Queries;
+
+namespace Together.ProductService.Api.V1
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    public class CategoryController : BaseController
+    {
+        [HttpGet("/api/v{version:apiVersion}/getCategoryById")]
+        public async Task<ActionResult> HandleGetCategoryByIdAsync(Guid id, CancellationToken cancellationToken = new())
+        {
+            var request = new GetCategoryById.Query { Id = id };
+            return Ok(await Mediator.Send(request, cancellationToken));
+        }
+
+        [HttpPost("/api/v{version:apiVersion}/createCategory")]
+        public async Task<ActionResult> HandleCreateCategoryAsync([FromBody] CreateCategory.Command request, CancellationToken cancellationToken = new())
+        {
+            return Ok(await Mediator.Send(request, cancellationToken));
+        }
+    }
+}
