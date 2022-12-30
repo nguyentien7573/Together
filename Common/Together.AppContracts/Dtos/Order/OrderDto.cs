5c679338bfbd0614dec4cfa3f49f1fc2d3816881
Nguyễn Tiến <61858121+nguyentien7573@users.noreply.github.com>
2022-12-30 15:37:59 +0700

    Order Dto

---
+++ Common/Together.AppContracts/Dtos/Order/OrderDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Together.AppContracts.Dtos.Order
+{
+    public class OrderDto
+    {
+        public Guid Id { get; set; }
+        public Guid CustomerId { get; set; }
+        public float TotalCost { get; set; }
+        public AddressDto Address { get; set; } = default!;
+        public string Status { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public List<OrderItemDto> OrderItems { get; set; } = default!;
+    }
+}
---
+++ Common/Together.AppContracts/Dtos/Order/OrderItemDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Together.AppContracts.Dtos.Order
+{
+    public class OrderItemDto
+    {
+        public int ProductId { get; private set; }
+        private decimal UnitPrice { get; set; }
+        private decimal Discount { get; set; }
+        private int Units { get; set; }
+    }
+}
