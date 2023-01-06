e9adb5b185cf86cde115f667b3cb2c1f6c73a997
Nguyễn Tiến <61858121+nguyentien7573@users.noreply.github.com>
2023-01-06 17:16:42 +0700

    Update Program.cs

--- MicroServices/Order/Together.OrderService.Api/Program.cs
+++ MicroServices/Order/Together.OrderService.Api/Program.cs
@@ -4,13 +4,13 @@ using Together.OrderService.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddCoreServices(builder.Configuration, builder.Environment, typeof(ApiAnchor));
 builder.Services.AddControllers();
+builder.Services.AddCoreServices(builder.Configuration, builder.Environment, typeof(ApiAnchor));
+
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
