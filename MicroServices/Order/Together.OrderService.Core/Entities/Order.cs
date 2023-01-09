e6503416912f96fae58372359d7be1785d2aa8e2
Nguyễn Tiến <61858121+nguyentien7573@users.noreply.github.com>
2023-01-09 14:35:10 +0700

    Add address properites

--- MicroServices/Order/Together.OrderService.Core/Entities/Order.cs
+++ MicroServices/Order/Together.OrderService.Core/Entities/Order.cs
@@ -7,9 +7,15 @@ namespace Together.OrderService.Core.Entities
     {
         public Guid CustomerId { get; set; }
         public float TotalCost { get; set; }
-        public string Address { get; set; } = default!;
         public string Status { get; set; } = default!;
         public string Description { get; set; } = default!;
+        public string DistrictCode { get; set; } = default!;
+        public string ProvincesCode { get; set; } = default!;
+        public string WardCode { get; set; } = default!;
+        public string Address { get; set; } = default!;
+        public string Address1 { get; set; } = default!;
+        public string Address2 { get; set; } = default!;
+
         public List<OrderItem> OrderItems { get; set; } = default!;
 
         public static Order Create(Guid customerId, float totalCost, string address, string status, string description, List<OrderItem> orderItems)
