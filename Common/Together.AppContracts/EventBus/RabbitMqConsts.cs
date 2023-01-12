4ee02b016215a316e41500299de6918d5b3d9048
Nguyễn Tiến <61858121+nguyentien7573@users.noreply.github.com>
2023-01-12 15:22:14 +0700

    Add common EventBus

---
+++ Common/Together.AppContracts/EventBus/RabbitMqConsts.cs
@@ -0,0 +1,11 @@
+﻿namespace Together.AppContracts.EventBus
+{
+    public class RabbitMqConsts
+    {
+        public const string RabbitMqRootUri = "rabbitmq://localhost";
+        public const string RabbitMqOrderUri = "rabbitmq://localhost/orderQueue";
+        public const string UserName = "guest";
+        public const string Password = "guest";
+        public const string NotificationServiceQueue = "notification.service";
+    }
+}
---
+++ Common/Together.Infrastructure/EventBus/Extentions.cs
@@ -0,0 +1,41 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Together.Infrastructure.EventBus
+{
+    public static class Extentions
+    {
+        public static IServiceCollection AddMassTransit(this IServiceCollection services)
+        {
+            services.AddMassTransit(x =>
+            {
+                x.UsingRabbitMq();
+            });
+
+            services.AddOptions<MassTransitHostOptions>()
+                .Configure(options =>
+                {
+                    // if specified, waits until the bus is started before
+                    // returning from IHostedService.StartAsync
+                    // default is false
+                    options.WaitUntilStarted = true;
+
+                    // if specified, limits the wait time when starting the bus
+                    options.StartTimeout = TimeSpan.FromSeconds(10);
+
+                    // if specified, limits the wait time when stopping the bus
+                    options.StopTimeout = TimeSpan.FromSeconds(30);
+                });
+            services.AddMassTransitHostedService();
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseMassTransit(this IApplicationBuilder app, IApiVersionDescriptionProvider provider)
+        {
+            return app;
+        }
+    }
+}
