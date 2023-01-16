610cad5f2b3c5ffdc84ab6d945a7732ad89a6de6
Nguyễn Tiến <61858121+nguyentien7573@users.noreply.github.com>
2023-01-16 17:00:03 +0700

    Create OrderConsumer.cs

---
+++ MicroServices/Order/Together.OrderService.Core/EventBus/OrderConsumer.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using MassTransit;
+using Together.AppContracts.Dtos.Order;
+using Together.Core.Repository;
+using Together.OrderService.Core.Entities;
+using Together.OrderService.Core.EntityConfig;
+
+namespace Together.OrderService.Core.EventBus
+{
+    public class OrderConsumer : IConsumer<OrderDto>
+    {
+        private readonly IRepository<Order> _orderRepository;
+
+      
+        public async Task Consume(ConsumeContext<OrderDto> context)
+        {
+
+            var request = context.Message;
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new MappingProfile());
+            });
+            var mapper = config.CreateMapper();
+            var orderItems = new List<OrderItem>();
+
+            if (request.OrderItems.Any())
+            {
+                foreach (var orderItem in request.OrderItems)
+                {
+                    orderItems.Add(mapper.Map<OrderItemDto,OrderItem>(orderItem));
+                }
+            }
+
+            var created = await _orderRepository.AddAsync(
+                      Order.Create(
+                          request.CustomerId,
+                          request.TotalCost,
+                          request.Address,
+                          request.Address1,
+                          request.Address2,
+                          request.ProvincesCode,
+                          request.WardCode,
+                          request.DistrictCode,
+                          request.Status,
+                          request.Description,
+                          orderItems));
+            throw new NotImplementedException();
+        }
+    }
+}
