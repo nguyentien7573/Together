1408ea2cf2db0468ba64ddd6cc6d342e8a3d0f26
Nguyễn Tiến <61858121+nguyentien7573@users.noreply.github.com>
2023-01-17 22:22:17 +0700

    Update ProductController.cs

--- MicroServices/Product/Together.ProductService.Api/V1/ProductController.cs
+++ MicroServices/Product/Together.ProductService.Api/V1/ProductController.cs
@@ -11,10 +11,19 @@ namespace Together.ProductService.Api.V1
     [ApiVersion("1.0")]
     public class ProductController : BaseController
     {
+
+        
         [HttpGet("/api/v{version:apiVersion}/product/{id:guid}")]
         public async Task<ActionResult<ProductDto>> GetByIdAsync(Guid id, CancellationToken cancellationToken = new())
         {
             var request = new GetProductById.Query { Id = id };
+            return Ok(await Mediator.Send(request, cancellationToken));
+        }
+
+        [HttpGet("/api/v{version:apiVersion}/getProductsByCategory")]
+        public async Task<ActionResult> GetProductByIdAsync(Guid id, CancellationToken cancellationToken = new())
+        {
+            var request = new GetProductByCategoryId.Query { Id = id };
             return Ok(await Mediator.Send(request, cancellationToken));
         }
 
@@ -24,10 +33,16 @@ namespace Together.ProductService.Api.V1
             return Ok(await Mediator.Send(request, cancellationToken));
         }
 
-        [HttpGet("/api/v{version:apiVersion}/getProductsByCategory")]
-        public async Task<ActionResult> GetAsync(Guid id, CancellationToken cancellationToken = new())
+        [HttpPut("/api/v{version:apiVersion}/updateProduct")]
+        public async Task<ActionResult> UpdateProductAsync([FromBody] UpdateProduct.Command request, CancellationToken cancellationToken = new())
         {
-            var request = new GetProductByCategoryId.Query { Id = id };
+            return Ok(await Mediator.Send(request, cancellationToken));
+        }
+
+        [HttpDelete("/api/v{version:apiVersion}/delteProduct")]
+        public async Task<ActionResult> DeleteProductAsync(Guid id, CancellationToken cancellationToken = new())
+        {
+            var request = new DeleteProduct.Command { Id = id };
             return Ok(await Mediator.Send(request, cancellationToken));
         }
     }
