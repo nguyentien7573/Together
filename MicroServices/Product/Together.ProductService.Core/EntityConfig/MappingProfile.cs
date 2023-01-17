b34cc4f328e6e3043c11c86371cef1f0500e8071
Nguyễn Tiến <61858121+nguyentien7573@users.noreply.github.com>
2023-01-17 19:26:23 +0700

    Mapper

---
+++ MicroServices/Product/Together.ProductService.Core/EntityConfig/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Together.AppContracts.Dtos.Product;
+using Together.ProductService.Core.Entities;
+
+namespace Together.ProductService.Core.EntityConfig
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<IEnumerable<Product>, IEnumerable<ProductDto>>();
+        }
+    }
+}
