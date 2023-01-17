d925318cf6d4d20d57c7217693f014585f7cfbd8
Nguyễn Tiến <61858121+nguyentien7573@users.noreply.github.com>
2023-01-17 11:00:30 +0700

    Update OrderItemDto.cs

--- Common/Together.AppContracts/Dtos/Order/OrderItemDto.cs
+++ Common/Together.AppContracts/Dtos/Order/OrderItemDto.cs
@@ -2,7 +2,7 @@
 {
     public class OrderItemDto : BaseDto
     {
-        public Guid ProductId { get; private set; }
+        public Guid ProductId { get; set; }
         public decimal UnitPrice { get; set; }
         public decimal Discount { get; set; }
         public int Units { get; set; }
