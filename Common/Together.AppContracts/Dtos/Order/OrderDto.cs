31423f53853674ae1ab99649963928dcbdddaeda
Nguyễn Tiến <61858121+nguyentien7573@users.noreply.github.com>
2023-01-19 15:47:55 +0700

    Update OrderDto.cs

--- Common/Together.AppContracts/Dtos/Order/OrderDto.cs
+++ Common/Together.AppContracts/Dtos/Order/OrderDto.cs
@@ -4,15 +4,13 @@
     {
         public Guid Id { get; set; }
         public Guid CustomerId { get; set; }
-        public float TotalCost { get; set; }
+        public decimal Total { get; set; }
         public string DistrictCode { get; set; } = default!;
         public string ProvincesCode { get; set; } = default!;
         public string WardCode { get; set; } = default!;
         public string Address { get; set; } = default!;
         public string Address1 { get; set; } = default!;
         public string Address2 { get; set; } = default!;
-        public string Status { get; set; } = default!;
-        public string Description { get; set; } = default!;
         public List<OrderItemDto> OrderItems { get; set; } = default!;
     }
 }
