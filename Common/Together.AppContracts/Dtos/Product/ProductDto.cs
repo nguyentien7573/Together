e4a49813c7e134dd24ee90c8809c9085aea6b254
Nguyễn Tiến <61858121+nguyentien7573@users.noreply.github.com>
2023-01-19 13:11:42 +0700

    ProductDto

--- Common/Together.AppContracts/Dtos/Product/ProductDto.cs
+++ Common/Together.AppContracts/Dtos/Product/ProductDto.cs
@@ -4,9 +4,12 @@
     {
         public Guid Id { get; set; }
         public string Name { get; set; } = default!;
-        public bool Active { get; set; }
-        public int Quantity { get; set; }
-        public decimal Cost { get; set; }
+        public string Description { get; set; } = default!;
+        public string SKU { get; set; } = default!;
         public Guid CategoryId { get; set; }
+        public Guid? InventoryId { get; set; }
+        public decimal Price { get; set; } = 0;
+        public Guid? DiscountId { get; set; }
+        public bool IsActive { get; set; }
     }
 }
