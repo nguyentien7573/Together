b686f739b07f67e886f3d0d3563360afd69dc731
Nguyễn Tiến <61858121+nguyentien7573@users.noreply.github.com>
2023-01-19 15:48:31 +0700

    Update Order.cs

--- MicroServices/Order/Together.OrderService.Core/Entities/Order.cs
+++ MicroServices/Order/Together.OrderService.Core/Entities/Order.cs
@@ -6,36 +6,30 @@ namespace Together.OrderService.Core.Entities
     public class Order : EntityBase
     {
         public Guid CustomerId { get; set; }
-        public float TotalCost { get; set; }
-        public string Status { get; set; } = default!;
-        public string Description { get; set; } = default!;
+        public decimal Total { get; set; }
+        public Guid PaymentId { get; set; }
         public string DistrictCode { get; set; } = default!;
         public string ProvincesCode { get; set; } = default!;
         public string WardCode { get; set; } = default!;
         public string Address { get; set; } = default!;
         public string Address1 { get; set; } = default!;
         public string Address2 { get; set; } = default!;
-
         public List<OrderItem> OrderItems { get; set; } = default!;
 
-        public static Order Create(Guid customerId, float totalCost, string address, string address1, 
-            string address2, string districtCode, string provincesCode, string wardCode,
-            string status, string description, List<OrderItem> orderItems)
+        public static Order Create(Guid customerId, decimal total, string address, string address1,
+            string address2, string districtCode, string provincesCode, string wardCode, List<OrderItem> orderItems)
         {
             Order order = new()
             {
                 CustomerId = customerId,
-                TotalCost = totalCost,
+                Total = total,
                 Address = address,
                 Address1 = address1,
-                Address2= address2,
+                Address2 = address2,
                 DistrictCode = districtCode,
                 ProvincesCode = provincesCode,
                 WardCode = wardCode,
-                Status = status,
-                Description = description,
                 OrderItems = orderItems
-               
             };
 
             return order;
