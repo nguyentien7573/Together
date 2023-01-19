92921c61569a90f70e9f5cbe738c9a0fc8f74027
Nguyễn Tiến <61858121+nguyentien7573@users.noreply.github.com>
2023-01-19 15:55:28 +0700

    Visual file

---
+++ MicroServices/Shopping/Together.Shopping.Infrastructor/Data/ShoppingtDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Together.Infrastructure.Data;
+using Together.Shopping.Core.Entities;
+
+namespace Together.Shopping.Infrastructor.Data
+{
+    public class ShoppingtDbContext : AppDbContextBase
+    {
+        private const string Schema = "shop";
+        public ShoppingtDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<ShoppingSession> ShoppingSessions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CartItem>().ToTable("Products", Schema);
+            modelBuilder.Entity<CartItem>().HasKey(x => x.Id);
+            modelBuilder.Entity<CartItem>().Property(x => x.Id).HasDefaultValueSql("NEWID()");
+
+            modelBuilder.Entity<ShoppingSession>().ToTable("Categories", Schema);
+            modelBuilder.Entity<ShoppingSession>().HasKey(x => x.Id);
+            modelBuilder.Entity<ShoppingSession>().Property(x => x.Id).HasDefaultValueSql("NEWID()");
+        }
+    }
+}
