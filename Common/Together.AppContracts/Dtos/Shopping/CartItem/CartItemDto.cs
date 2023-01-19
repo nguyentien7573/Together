340ef83c21d0448a8f3c374e1d663c15d06015ea
Nguyễn Tiến <61858121+nguyentien7573@users.noreply.github.com>
2023-01-19 15:48:08 +0700

    Create CartItemDto.cs

---
+++ Common/Together.AppContracts/Dtos/Shopping/CartItem/CartItemDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Together.AppContracts.Dtos.Shopping.CartItem
+{
+    public class CartItemDto : BaseDto
+    {
+        public Guid Id { get; set; }
+        public Guid SessionId { get; set; }
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
