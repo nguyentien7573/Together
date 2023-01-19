7a980b158be9fa06c741e9eafe1f4d50c13cf9af
Nguyễn Tiến <61858121+nguyentien7573@users.noreply.github.com>
2023-01-19 15:54:51 +0700

    Repository

---
+++ MicroServices/Shopping/Together.Shopping.Infrastructor/Repositories/CartItemRepository.cs
@@ -0,0 +1,9 @@
+﻿using Together.Infrastructure.Data;
+using Together.Shopping.Core.Entities;
+
+namespace Together.Shopping.Infrastructor.Repositories
+{
+    public class CartItemRepository : RepositoryBase<ProductDbContext, CartItem>
+    {
+    }
+}
---
+++ MicroServices/Shopping/Together.Shopping.Infrastructor/Repositories/ShoppingSessionRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Together.Infrastructure.Data;
+using Together.Shopping.Core.Entities;
+using Together.Shopping.Infrastructor.Data;
+
+namespace Together.Shopping.Infrastructor.Repositories
+{
+    public class ShoppingSessionRepository : RepositoryBase<ShoppingtDbContext, ShoppingSession>
+    {
+        public ShoppingSessionRepository(ShoppingtDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
