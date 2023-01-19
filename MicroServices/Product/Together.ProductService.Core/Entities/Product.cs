b0b475b9de197b9d9e8b9e620f8f4baddf8cd0f6
Nguyễn Tiến <61858121+nguyentien7573@users.noreply.github.com>
2023-01-19 11:46:07 +0700

    Product entity

--- MicroServices/Product/Together.ProductService.Core/Entities/Product.cs
+++ MicroServices/Product/Together.ProductService.Core/Entities/Product.cs
@@ -5,19 +5,25 @@ namespace Together.ProductService.Core.Entities
     public partial class Product : EntityBase
     {
         public string Name { get; set; } = default!;
-        public int Quantity { get; set; }
-        public decimal Cost { get; set; }
+        public string Description { get; set; } = default!;
+        public string SKU { get; set; } = default!;
         public Guid CategoryId { get; set; }
+        public Guid? InventoryId { get; set; }
+        public decimal Price { get; set; } = 0;
+        public Guid? DiscountID { get; set; }
 
-        public static Product Create(string name, int quantity, decimal cost, Guid categoryId, bool Active)
+        public static Product Create(string name, string description, string sku, Guid categoryId, Guid inventoryId, decimal price, Guid discountID, bool isActive)
         {
             Product product = new()
             {
                 Name = name,
-                Quantity = quantity,
-                Cost = cost,
+                Description = description,
+                SKU = sku,
                 CategoryId = categoryId,
-                Active = Active,
+                InventoryId = inventoryId,
+                Price = price, 
+                DiscountID = discountID,
+                IsActive = isActive
             };
 
             return product;
