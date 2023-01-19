5afa0ef0fa902ca82c05f7e52ac80f33a9b1e619
Nguyễn Tiến <61858121+nguyentien7573@users.noreply.github.com>
2023-01-19 15:54:35 +0700

    New Entity for Shopping

---
+++ MicroServices/Shopping/Together.Shopping.Core/Entities/CartItem.cs
@@ -0,0 +1,17 @@
+﻿using Together.Core.Domain;
+
+namespace Together.Shopping.Core.Entities
+{
+    public class CartItem : EntityBase
+    {
+        public Guid SessionId { get; set; }
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+
+        public static CartItem Create(Guid sessionId, Guid productId, int quantity)
+        {
+            return new() { SessionId = sessionId, ProductId = productId, Quantity = quantity};
+        }
+        
+    }
+}
---
+++ MicroServices/Shopping/Together.Shopping.Core/Entities/ShoppingSession.cs
@@ -0,0 +1,17 @@
+﻿using Together.Core.Domain;
+
+namespace Together.Shopping.Core.Entities
+{
+    public class ShoppingSession : EntityBase
+    {
+        public Guid CustomerId { get; set; }
+        public decimal Total { get; set; }
+        public List<CartItem> CartItems { get; set; }
+
+        public static ShoppingSession Create(Guid customerId, decimal total, List<CartItem> cartItems)
+        {
+            return new() { CartItems = cartItems, CustomerId = customerId, Total = total };
+
+        }
+    }
+}
