be7bcc8e0e6425236d9f95b186ae06e49580baa1
Nguyễn Tiến <61858121+nguyentien7573@users.noreply.github.com>
2023-01-30 18:50:21 +0700

    Create Extensions.cs

---
+++ MicroServices/Shopping/Together.Shopping.Infrastructor/Extensions.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Together.Core.Repository;
+using Together.Infrastructure;
+using Together.Infrastructure.Swagger;
+using Together.Infrastructure.Validator;
+using Together.Shopping.Core.Entities;
+using Together.Shopping.Infrastructor.Data;
+using Together.Shopping.Infrastructor.Repositories;
+using AppCoreAnchor = Together.Shopping.Core.Anchor;
+
+namespace Together.Shopping.Infrastructor
+{
+    public static class Extensions
+    {
+        private const string CorsName = "api";
+
+        public static IServiceCollection AddCoreServices(this IServiceCollection services, IConfiguration config, IWebHostEnvironment env, Type apiType)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(CorsName, policy =>
+                {
+                    policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                });
+            });
+
+            services.AddHttpContextAccessor();
+            services.AddCustomMediatR(new[] { typeof(AppCoreAnchor) });
+            services.AddCustomValidators(new[] { typeof(AppCoreAnchor) });
+            services.AddControllers();
+            services.AddSwagger(apiType);
+
+            services.AddDbContext<ShoppingtDbContext>(options => options.UseSqlServer(config.GetConnectionString("defaultConnection")));
+
+            services.AddScoped<IRepository<CartItem>, CartItemRepository>();
+            services.AddScoped<IRepository<ShoppingSession>, ShoppingSessionRepository>();
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseCoreApplication(this WebApplication app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(CorsName);
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers()
+                    .RequireAuthorization("ApiCaller");
+            });
+
+            var provider = app.Services.GetService<IApiVersionDescriptionProvider>();
+            return app.UseSwagger(provider);
+        }
+    }
+}
