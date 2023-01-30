bce4688e5810f118bedba57217b476369adbe80b
Nguyễn Tiến <61858121+nguyentien7573@users.noreply.github.com>
2023-01-30 18:50:27 +0700

    Update CartItemRepository.cs

--- MicroServices/Shopping/Together.Shopping.Infrastructor/Repositories/CartItemRepository.cs
+++ MicroServices/Shopping/Together.Shopping.Infrastructor/Repositories/CartItemRepository.cs
@@ -1,9 +1,13 @@
 ﻿using Together.Infrastructure.Data;
 using Together.Shopping.Core.Entities;
+using Together.Shopping.Infrastructor.Data;
 
 namespace Together.Shopping.Infrastructor.Repositories
 {
-    public class CartItemRepository : RepositoryBase<ProductDbContext, CartItem>
+    public class CartItemRepository : RepositoryBase<ShoppingtDbContext, CartItem>
     {
+        public CartItemRepository(ShoppingtDbContext dbContext) : base(dbContext)
+        {
+        }
     }
 }
