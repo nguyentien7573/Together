52f0e76d39bf543f484133f8cc283f1e48211935
Nguyễn Tiến <61858121+nguyentien7573@users.noreply.github.com>
2023-01-30 18:49:25 +0700

    Shopping Api

---
+++ MicroServices/Shopping/Together.Shopping.Api/V1/CartController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Together.AppContracts.Dtos.Shopping.CartItem;
+using Together.Infrastructure.Controller;
+using Together.Shopping.Core.UseCases.Commands.CartItems;
+
+namespace Together.Shopping.Api.V1
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    public class CartController : BaseController
+    {
+        [HttpPost("/api/v{version:apiVersion}/createCart/")]
+        public async Task<ActionResult<CartItemDto>> AddAsync([FromBody] InsertCartItem.Command request, CancellationToken cancellationToken = new())
+        {
+            return Ok(await Mediator.Send(request, cancellationToken));
+        }
+
+        [HttpPut("/api/v{version:apiVersion}/updateCart/")]
+        public async Task<ActionResult<CartItemDto>> UpdateAsync([FromBody] UpdateCartItem.Command request, CancellationToken cancellationToken = new())
+        {
+            return Ok(await Mediator.Send(request, cancellationToken));
+        }
+
+        [HttpDelete("/api/v{version:apiVersion}/deleteCart")]
+        public async Task<ActionResult<CartItemDto>> GetAsync(Guid Id, CancellationToken cancellationToken = new())
+        {
+            var request = new DeleteCartItem.Command { Id = Id };
+            return Ok(await Mediator.Send(request, cancellationToken));
+        }
+    }
+}
---
+++ MicroServices/Shopping/Together.Shopping.Api/V1/ShoppingController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Together.AppContracts.Dtos.Shopping.Shopping;
+using Together.Infrastructure.Controller;
+using Together.Shopping.Core.UseCases.Commands.ShoppingSessions;
+
+namespace Together.Shopping.Api.V1
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    public class ShoppingController : BaseController
+    {
+        [HttpPost("/api/v{version:apiVersion}/createShoppingSession/")]
+        public async Task<ActionResult<ShoppingSessionDto>> AddAsync([FromBody] InsertShoppingSession.Command request, CancellationToken cancellationToken = new())
+        {
+            return Ok(await Mediator.Send(request, cancellationToken));
+        }
+
+        [HttpPut("/api/v{version:apiVersion}/updateShoppingSession/")]
+        public async Task<ActionResult<ShoppingSessionDto>> UpdateAsync([FromBody] UpdateShoppingSessions.Command request, CancellationToken cancellationToken = new())
+        {
+            return Ok(await Mediator.Send(request, cancellationToken));
+        }
+
+        [HttpDelete("/api/v{version:apiVersion}/deleteShoppingSession")]
+        public async Task<ActionResult<ShoppingSessionDto>> GetAsync(Guid Id, CancellationToken cancellationToken = new())
+        {
+            var request = new DeleteShoppingSessions.Command { Id = Id };
+            return Ok(await Mediator.Send(request, cancellationToken));
+        }
+    }
+}
