7f71c2d52cd931b32d520b4387ad4f048955d334
Nguyễn Tiến <61858121+nguyentien7573@users.noreply.github.com>
2023-01-30 18:49:52 +0700

    Create ShoppingSessionDto.cs

---
+++ Common/Together.AppContracts/Dtos/Shopping/Shopping/ShoppingSessionDto.cs
@@ -0,0 +1,12 @@
+﻿using Together.AppContracts.Dtos.Shopping.CartItem;
+
+namespace Together.AppContracts.Dtos.Shopping.Shopping
+{
+    public class ShoppingSessionDto : BaseDto
+    {
+        public Guid Id { get; set; }
+        public Guid CustomerId { get; set; }
+        public decimal Total { get; set; }
+        public List<CartItemDto> CartItems { get; set; }
+    }
+}
