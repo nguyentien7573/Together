02eaf616443b2c15c5120a0a99c9140dd54dcf90
Nguyễn Tiến <61858121+nguyentien7573@users.noreply.github.com>
2023-01-30 18:49:48 +0700

    Update CustomerDto.cs

--- Common/Together.AppContracts/Dtos/Customer/CustomerDto.cs
+++ Common/Together.AppContracts/Dtos/Customer/CustomerDto.cs
@@ -3,8 +3,8 @@
     public class CustomerDto : BaseDto
     {
         public Guid Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public string Email { get; set; } = default!;
     }
 }
